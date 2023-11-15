f24624e80dad746b9c16f6712f09f01b0560b43d
rnimisha <nimisaraut@gmail.com>
2023-11-15 23:07:22 +0545

    feat: extract necessary keys from data

--- scripts/etl/extract.py
+++ scripts/etl/extract.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from scripts.utils.generate_spotify_client import generate_spotify_client
 
 
@@ -8,4 +10,25 @@ def extract_spotify_recently_played():
 
     print(len(recently_played["items"]))
 
-    return recently_played["items"]
+    played_track_data = []
+
+    for item in recently_played["items"]:
+        track = item["track"]
+
+        track_data_dict = {
+            "song_title": track.get("name", "N/A"),
+            "artist_name": track["album"]["artists"][0].get("name", "N/A"),
+            "played_at": item.get("played_at", "N/A"),
+            "song_duration_ms": track.get("duration_ms", "N/A"),
+        }
+        played_track_data.append(track_data_dict)
+
+    df_columns = ["song_title", "artist_name", "played_at", "song_duration_ms"]
+    played_track_df = pd.DataFrame(played_track_data, columns=df_columns)
+
+    csv_filename = "staging_played_tracks.csv"
+    played_track_df.to_csv(csv_filename, index=False)
+
+    print(f"DataFrame saved to {csv_filename}")
+
+    return played_track_df
