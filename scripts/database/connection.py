b6903748dd342ae560d4c7f917666b7ab4f13221
rnimisha <nimisaraut@gmail.com>
2023-11-17 00:01:42 +0545

    feat: create connection to postgres

---
+++ scripts/database/connection.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+from config.config import app_config
+
+
+def connect_to_database():
+    try:
+        conn = psycopg2.connect(
+            dbname=app_config.get_db_name(),
+            user=app_config.get_db_user(),
+            password=app_config.get_db_password(),
+            host=app_config.get_db_host(),
+            port=app_config.get_db_port(),
+        )
+
+        print("Connected to database")
+        return conn
+
+    except Exception as e:
+        print("Error connecting to database: ", e)
+        return None
