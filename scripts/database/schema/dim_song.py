01f3ec6c2e961942ab229d31e7f184763ad73a04
rnimisha <nimisaraut@gmail.com>
2023-11-17 19:24:29 +0545

    fix: solve syntax issue of sql

--- scripts/database/schema/dim_song.py
+++ scripts/database/schema/dim_song.py
@@ -2,7 +2,7 @@ def get_dim_song_create_query():
     query = """
         CREATE TABLE IF NOT EXISTS dim_song (
             song_id SERIAL PRIMARY KEY,
-            song_title VARCHAR(255)
+            song_title VARCHAR(255),
             duration_ms INTEGER
         )
     """
