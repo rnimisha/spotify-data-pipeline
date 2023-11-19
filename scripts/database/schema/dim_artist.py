53328a927c80d64d282a812cfd182ffe91f24201
rnimisha <nimisaraut@gmail.com>
2023-11-19 23:33:59 +0545

    feat: add unique constraint and index on artist_name

--- scripts/database/schema/dim_artist.py
+++ scripts/database/schema/dim_artist.py
@@ -2,7 +2,8 @@ def get_dim_artist_create_query():
     query = """
         CREATE TABLE IF NOT EXISTS dim_artist (
             artist_id SERIAL PRIMARY KEY,
-            artist_name VARCHAR(255)
-        )
+            artist_name VARCHAR(255) UNIQUE
+        );
+        CREATE INDEX IF NOT EXISTS index_artist_name ON dim_artist(artist_name);
     """
     return query
