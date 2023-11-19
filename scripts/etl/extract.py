d1dd66c092af8ba0b02da89a1280ad673774ed92
rnimisha <nimisaraut@gmail.com>
2023-11-19 23:47:30 +0545

    fix: add natual keys when extracting data

--- scripts/etl/extract.py
+++ scripts/etl/extract.py
@@ -17,11 +17,13 @@ def extract_single_track_data(track_item):
         "artist_name": track["album"]["artists"][0].get("name", "N/A"),
         "played_at": track_item.get("played_at", "N/A"),
         "song_duration_ms": track.get("duration_ms", "N/A"),
+        "artist_natural_key": track["album"]["artists"][0].get("id"),
+        "song_natural_key": track.get("id"),
     }
     return track_data_dict
 
 
-def extract_spotify_recently_played():
+def extract_spotify_recently_played() -> pd.DataFrame:
     """
     Fetch recently played songs from spotify playlist.
     Extract relevat track information.
@@ -42,7 +44,14 @@ def extract_spotify_recently_played():
             extract_single_track_data(item) for item in recently_played["items"]
         ]
 
-        df_columns = ["song_title", "artist_name", "played_at", "song_duration_ms"]
+        df_columns = [
+            "song_title",
+            "artist_name",
+            "played_at",
+            "song_duration_ms",
+            "artist_natural_key",
+            "song_natural_key",
+        ]
         played_track_df = pd.DataFrame(played_track_data, columns=df_columns)
 
         return played_track_df
@@ -52,10 +61,18 @@ def extract_spotify_recently_played():
         return pd.DataFrame()
 
 
-def save_to_staging_csv(data_frame):
+def save_to_staging_csv(df: pd.DataFrame):
     """
     Store extracted data to staging layer as csv file.
     """
     csv_filename = "staging_played_tracks.csv"
     csv_path = os.path.join("data/staging", csv_filename)
-    save_df_as_csv(data_frame, csv_path)
+    save_df_as_csv(df, csv_path)
+
+
+def extract():
+    try:
+        df = extract_spotify_recently_played()
+        save_to_staging_csv(df)
+    except Exception as e:
+        print(e)
