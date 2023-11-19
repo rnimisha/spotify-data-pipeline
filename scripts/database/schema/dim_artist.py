6fb0c3539da63b1b917522834db63f822d386f15
rnimisha <nimisaraut@gmail.com>
2023-11-19 23:55:51 +0545

    fix: add natural key in schema with index

--- scripts/database/schema/dim_artist.py
+++ scripts/database/schema/dim_artist.py
@@ -2,8 +2,9 @@ def get_dim_artist_create_query():
     query = """
         CREATE TABLE IF NOT EXISTS dim_artist (
             artist_id SERIAL PRIMARY KEY,
-            artist_name VARCHAR(255) UNIQUE
+            artist_name VARCHAR(255),
+            artist_natural_key VARCHAR(255) UNIQUE
         );
-        CREATE INDEX IF NOT EXISTS index_artist_name ON dim_artist(artist_name);
+        CREATE INDEX IF NOT EXISTS index_artist_natural_key ON dim_artist(artist_natural_key);
     """
     return query
--- scripts/database/schema/dim_song.py
+++ scripts/database/schema/dim_song.py
@@ -3,7 +3,9 @@ def get_dim_song_create_query():
         CREATE TABLE IF NOT EXISTS dim_song (
             song_id SERIAL PRIMARY KEY,
             song_title VARCHAR(255),
-            duration_ms INTEGER
-        )
+            duration_ms INTEGER,
+            song_natural_key VARCHAR(255) UNIQUE
+        );
+        CREATE INDEX IF NOT EXISTS index_song_natural_key ON dim_song(song_natural_key);
     """
     return query
