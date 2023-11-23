181b4694565fde488ea6371eaf789769bb34e154
rnimisha <nimisaraut@gmail.com>
2023-11-24 00:54:01 +0545

    feat: use remote selenium  web driver

--- scripts/utils/automation/web_driver.py
+++ scripts/utils/automation/web_driver.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 def get_webdriver():
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
 
     chrome_options.add_argument("--window-size=1920,1080")
@@ -11,5 +11,6 @@ def get_webdriver():
         "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
     )
 
-    driver = webdriver.Chrome(options=chrome_options)
+    remote_webdriver = "remote_chromedriver"
+    driver = webdriver.Remote(f"{remote_webdriver}:4444/wd/hub", options=chrome_options)
     return driver
