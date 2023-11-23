ee5ede1fa8a1e9bd535d75f0a3ab7198c7b718da
rnimisha <nimisaraut@gmail.com>
2023-11-23 22:17:07 +0545

    feat: implement task api for dags

--- airflow/dags/spotify-etl-dag.py
+++ airflow/dags/spotify-etl-dag.py
@@ -3,46 +3,45 @@ from datetime import datetime, timedelta
 
 sys.path.append("/opt")
 
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.decorators import dag
 from scripts.database.create_table import create_star_schema_table
-from scripts.etl.extract import extract
+from scripts.etl.extract import extract_spotify_recently_played, save_to_staging_csv
 from scripts.etl.load import load
-from scripts.etl.transform import transform
+from scripts.etl.transform import save_df_to_processed_csv, transform
 
 default_args = {
     "owner": "nayeon",
     "depends_on_past": False,
-    "start_date": datetime(2023, 11, 20),
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
 
 
-with DAG(
-    "etl_spotify_data_pipeline",
-    default_args=default_args,
-    description="Data pipeline for ETL DAG for Spotify data",
+@dag(
     schedule_interval="@daily",
-    # schedule_interval=timedelta(minutes=1),
-) as dag:
-    create_tables = PythonOperator(
-        task_id="create_tables",
-        python_callable=create_star_schema_table,
-    )
-    extracts = PythonOperator(
-        task_id="extract",
-        python_callable=extract,
-    )
-    transforms = PythonOperator(
-        task_id="transform",
-        python_callable=transform,
-        provide_context=True,
-        op_kwargs={"filename": "staging_played_tracks.csv"},
-    )
-    loads = PythonOperator(
-        task_id="load",
-        python_callable=load,
+    start_date=datetime(2023, 11, 20),
+    catchup=False,
+    tags=["spotify_etl"],
+    description="Data pipeline for ETL DAG for Spotify data",
+    default_args=default_args,
+)
+def etl_spotify_data_pipeline():
+    create_table_task = create_star_schema_table()
+
+    extract_task = extract_spotify_recently_played()
+    save_to_staging_task = save_to_staging_csv(extract_task)
+    transform_task = transform("staging_played_tracks.csv")
+    save_to_processed_task = save_df_to_processed_csv(transform_task)
+    load_task = load()
+
+    (
+        create_table_task
+        >> extract_task
+        >> save_to_staging_task
+        >> transform_task
+        >> save_to_processed_task
+        >> load_task
     )
 
-create_tables >> extracts >> transforms >> loads
+
+etl_spotify_data_pipeline_dag = etl_spotify_data_pipeline()
